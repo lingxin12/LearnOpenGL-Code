9ceb54b8099dce8b9bb75923b8464f96fac5bb15
lingxin12 <1693662418@qq.com>
2023-03-03 17:20:24 +0800

    Update include/ glm/ and shader.h and stb_image.h

--- include/shader.hpp
+++ include/shader.hpp
@@ -9,6 +9,10 @@
 #include <sstream>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 class Shader
 {
@@ -21,6 +25,7 @@ public:
     void setBool(const std::string &name, bool value) const;  
     void setInt(const std::string &name, int value) const;   
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
 private:
     unsigned int ID;
@@ -111,13 +116,20 @@ void Shader::setBool(const std::string &name, bool value) const
 {
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
 }
+
 void Shader::setInt(const std::string &name, int value) const
 { 
     glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
+
 void Shader::setFloat(const std::string &name, float value) const
 { 
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 } 
+
+void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
 
 #endif
